fa63e95a593b617aedb08b3885d3b45e2fa2e864
racingsoftware2511 <racingsoftware@gmail.com>
2025-09-02 16:51:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -659,6 +659,7 @@ with tab5:
             (w["Attack_A_CF"] >= 60.0) &
             (w["Defense_H_CG"] <= 40.0) &
             (w["Wins_A_CP"] >= 70.0)
+            (w["AwayOdds_M"] <= 4.0)
         )
         backaway = w.loc[filt].copy()
 
